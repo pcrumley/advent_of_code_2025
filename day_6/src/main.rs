f7898c021eaa290806e3cfa50204488dac835f6b
Patrick Crumley <patrick.crumley@gmail.com>
2025-12-06 19:38:52 -0800

    solved day 6

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 use strum_macros::EnumString;
 
-#[derive(Debug, Clone, EnumString)]
+#[derive(Debug, Clone, Copy, EnumString)]
 enum Operation {
     #[strum(serialize = "+")]
     Add,
@@ -12,27 +12,60 @@ enum Operation {
     Mult,
 }
 
+enum Part {
+    A,
+    B,
+}
+
 #[derive(Debug, Clone)]
 struct Problem {
-    args: Vec<u64>,
+    args: Vec<String>,
     op: Operation,
 }
 
 impl Problem {
-    fn new(input: &[String]) -> Self {
+    fn new(inputs: &[String]) -> Self {
         let mut args = vec![];
-        for i in 0..input.len() - 1 {
-            let v = &input[i];
-            args.push(v.parse().unwrap());
+        for i in 0..inputs.len() - 1 {
+            args.push(inputs[i].clone());
         }
-        let op = input[input.len() - 1].parse().unwrap();
+        let op = inputs[inputs.len() - 1].trim().parse().unwrap();
+
         Self { args, op }
     }
-    fn solve(&self) -> u64 {
+
+    fn solve_a(&self) -> u64 {
         match self.op {
-            Operation::Mult => self.args.iter().fold(1, |acc, x| acc * x),
-            Operation::Add => self.args.iter().fold(0, |acc, x| acc + x),
+            Operation::Mult => self
+                .args
+                .iter()
+                .fold(1, |acc, x| acc * x.trim().parse::<u64>().unwrap()),
+            Operation::Add => self
+                .args
+                .iter()
+                .fold(0, |acc, x| acc + x.trim().parse::<u64>().unwrap()),
         }
+    }
+
+    fn solve_b(&self) -> u64 {
+        // have to take the transpose of all the strings first
+        let bytes_arr: Vec<&[u8]> = self.args.iter().map(|s| s.as_bytes()).collect();
+        let mut transposed_bytes_arr = vec![vec![b'0'; bytes_arr.len()]; bytes_arr[0].len()];
+
+        for i in 0..bytes_arr.len() {
+            for j in 0..bytes_arr[0].len() {
+                transposed_bytes_arr[j][i] = bytes_arr[i][j];
+            }
+        }
+
+        Self {
+            args: transposed_bytes_arr
+                .into_iter()
+                .map(|b| String::from_utf8(b).unwrap())
+                .collect(),
+            op: self.op,
+        }
+        .solve_a()
     }
 }
 
@@ -42,15 +75,47 @@ struct Worksheet {
 }
 
 impl Worksheet {
+    // this is helper function which just reads the last name of the file
+    // and gets the offsets
+    fn get_offsets(fname: &Path) -> Vec<usize> {
+        let f = File::open(fname).unwrap();
+        let reader = BufReader::new(f);
+
+        let last_line = reader.lines().last().unwrap().unwrap();
+        // get the offsets by reading the location of the chars that can be read
+        // in as a operator in the last line of the file
+        // we know its ascii so can be lazy about utf-8
+        let mut offsets: Vec<usize> = last_line
+            .bytes()
+            .enumerate()
+            .filter_map(|(i, b)| {
+                let as_str = str::from_utf8(std::slice::from_ref(&b)).ok()?;
+                let _: Operation = as_str.parse().ok()?;
+                Some(i)
+            })
+            .collect();
+
+        offsets.push(last_line.len() + 1);
+        offsets
+    }
     fn from_file(fname: impl AsRef<Path>) -> Self {
+        let offsets = Self::get_offsets(fname.as_ref());
         let f = File::open(fname.as_ref()).unwrap();
         let reader = BufReader::new(f);
 
         let mut input_matrix: Vec<Vec<String>> = vec![];
+
         for line in reader.lines() {
             let line = line.unwrap();
+            let line_bytes = line.into_bytes();
             // parse all the columns, effectively
-            let inputs = line.trim().split_whitespace().map(Into::into).collect();
+            let inputs = offsets
+                .iter()
+                .zip(offsets[1..].iter())
+                .map(|(&start, &next_start)| {
+                    String::from_utf8(line_bytes[start..next_start - 1].to_owned()).unwrap()
+                })
+                .collect();
             input_matrix.push(inputs);
         }
 
@@ -64,41 +129,37 @@ impl Worksheet {
             }
         }
 
-        /*
-        let problems = transposed_matrix
-            .iter()
-            .into_iter()
-            .map(|builder| builder.build())
-            .collect();
-        */
-        let problems = vec![];
+        let problems = transposed_matrix.iter().map(|m| Problem::new(&m)).collect();
         Self { problems }
+    }
+
+    fn sum(&self, part: Part) -> u64 {
+        match part {
+            Part::A => self.problems.iter().map(|p| p.solve_a()).sum(),
+            Part::B => self.problems.iter().map(|p| p.solve_b()).sum(),
+        }
     }
 }
 
 fn main() {
-    /*
-    let pantry = IngredientPantry::from_file("input.txt");
-    println!("Part A: `{}`", pantry.num_fresh_available());
-    println!("Part B: `{}`", pantry.num_fresh());
-    */
+    let worksheet = Worksheet::from_file("input.txt");
+    println!("Part A: `{}`", worksheet.sum(Part::A));
+    println!("Part B: `{}`", worksheet.sum(Part::B));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    /*
     #[test]
     fn test_a() {
-        let pantry = IngredientPantry::from_file("test.txt");
-        assert_eq!(3, pantry.num_fresh_available())
+        let ws = Worksheet::from_file("test.txt");
+        assert_eq!(4277556, ws.sum(Part::A))
     }
 
     #[test]
     fn test_b() {
-        let pantry = IngredientPantry::from_file("test.txt");
-        assert_eq!(14, pantry.num_fresh())
+        let ws = Worksheet::from_file("test.txt");
+        assert_eq!(3263827, ws.sum(Part::B))
     }
-    */
 }
